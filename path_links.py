4e579d9f7bc339c4a5f44c243ff4061fdda5fcef
murtazanasafi <nasafi.murtaza@gmail.com>
2018-04-13 10:34:00 -0400

    V2 fixed buts

--- path_links.py
+++ path_links.py
@@ -7,7 +7,7 @@ fips_table = r"E:\MFII_Dec2016\MFII_python_codes\CSVs\state FiPS.txt"
 num_provider_per_state = r"D:\FCC_GIS_Projects\MFII\DataCollection\Final_eligible_area_v8\input\LTE5_number_of_providers_per_state.csv"
 
 
-provider_coverages = r"D:\FCC_GIS_Projects\MFII\DataCollection\Final_eligible_area_v9\version_7_9\provider_coverages.gdb"
+provider_coverages = r"D:\FCC_GIS_Projects\Request_from_outside_WTB\OCH\input\V7_coverages.gdb"
 
-input_base_folder = r"D:\FCC_GIS_Projects\Request_from_outside_WTB\OCH\input"
-output_base_folder =r"D:\FCC_GIS_Projects\Request_from_outside_WTB\OCH\output"
+input_base_folder = r"D:\FCC_GIS_Projects\Request_from_outside_WTB\OCH\v2\input"
+output_base_folder =r"D:\FCC_GIS_Projects\Request_from_outside_WTB\OCH\v2\output"
